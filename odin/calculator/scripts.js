0b621494e64c562934f90f20747e93e02f5b0b90
Gabriel Siqueira <gssiqueira92@gmail.com>
2025-10-30 17:18:38 -0300

    add initial button functions

--- odin/calculator/scripts.js
+++ odin/calculator/scripts.js
@@ -1,19 +1,53 @@
-let numberA = 0;
-let numberB = 0;
+let numbers = {
+    a: "",
+    b: ""
+};
 let operator = "";
+let currentNumber = "a";
 
-function operate(operator,a,b){
-    switch(operator){
+function updateDisplay() {
+    const display = document.querySelector("#display h2");
+    display.textContent = numbers[currentNumber];
+}
+
+function clear() {
+    numbers = {
+        a: "",
+        b: ""
+    };
+    operator = "";
+    currentNumber = "a";
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
         case "+":
-            return a+b;
+            return a + b;
         case "-":
-            return a-b;
+            return a - b;
         case "*":
-            return a*b;
+            return a * b;
         case "/":
-            return a/b;
+            return a / b;
         default:
             return 0;
     }
 }
+
+const numbersPanel = document.querySelector("#numbersPanel");
+numbersPanel.addEventListener("click", (e) => {
+    if (e.target.classList.contains("number")) {
+        let newNumber = e.target.textContent;
+        if (numbers[currentNumber] !== "") {
+            newNumber = numbers[currentNumber].concat(newNumber);
+        }
+        numbers = {
+            ...numbers,
+            [currentNumber]: newNumber
+        }
+        updateDisplay();
+    }
+});
+
+
 
